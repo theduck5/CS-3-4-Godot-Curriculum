1e16f28da9294f5035890606b6817053509ca03b
theduck5 <foxyfamily111@gmail.com>
2025-10-28 13:44:13 -0700

    gougj

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -1,8 +1,21 @@
-@tool
+extends Node 
 
-extends  Resource
-class_name inventory_item
+@export var inventory: Array[inventory_item]
 
-@export var item_name: String
-@export var value: int
-@export var description: String
+var selected_item: int = 0
+
+func print_inventory():
+	if inventory.size()!= 0:
+		print(inventory.get(selected_item).item_name)
+		print(inventory.get(selected_item).description)
+		print(inventory.get(selected_item).value)
+		print(inventory.get(selected_item).amount)
+
+func _process(delta: float) -> void:
+	pass
+
+func add_item():
+	pass
+
+func remove_item():
+	pass
---
+++ scripts/inventoryitem.gd
@@ -0,0 +1,9 @@
+@tool
+
+extends  Resource
+class_name inventory_item
+
+@export var item_name: String
+@export var value: int
+@export var description: String
+@export var amount: int 
